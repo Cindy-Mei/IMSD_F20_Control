92337b89d727a78fffc552d223db0dd2e7e2550b
Cindy Mei <cindy.mei1@mail.citytech.cuny.edu>
2020-09-20 17:42:14 -0400

    really late part2 of  1st assignment

---
+++ IMSD_F20_Control_CindyMei/Assets/determineDiscontiguouslyv2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class determineSmoothv2 : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float speed;
+    public float x;
+    public float y;
+    public float z;
+
+    private float timer;
+
+    void Start()
+    {
+          myTranform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ IMSD_F20_Control_CindyMei/Assets/randomDiscontiguouslyv2.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class determineSmoothv2 : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float speed;
+    public float x;
+    public float y;
+    public float z;
+
+    private float timer;
+
+    void Start()
+    {
+          myTranform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomDiscontiguouslyv2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
