83e1c9ef1be120d069c562d9188172f32c810aad
Cindy Mei <cindy.mei1@mail.citytech.cuny.edu>
2020-09-20 18:14:36 -0400

    late is still late, and completely generic

---
+++ IMSD_F20_Control_CindyMei/Assets/determinedDiscontiguouslyV2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class determinedDiscontiguouslyV2 : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float x;
+    public float y;
+    public float z;
+
+    public float speed;
+
+    private float timer;
+
+    void Start()
+    {
+        myTransform.position = new Vector3(0, 0, 0);
+        timer = 0;
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= 1){
+          myTransform.position += new Vector3(Random.Range(-(x), x), y, z) * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ IMSD_F20_Control_CindyMei/Assets/randomDiscontiguouslyV2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomDiscontiguouslyV2 : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float rangeX;
+    public float rangeY;
+    public float rangeZ;
+
+    public float speed;
+
+    void Start()
+    {
+        myTransform.position = new  Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position += new Vector3(Random.Range(0, rangeX), Random.Range(0, rangeY), Random.Range(0, rangeZ)) * speed;
+    }
+}
